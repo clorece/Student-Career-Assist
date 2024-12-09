3c61b8eab5fa9956691de8b41b1f43dfcabbfde5
faith-i-m <fmadukwe@charlotte.edu>
2024-12-08 21:26:21 -0500

    register

--- django code/base/views.py
+++ django code/base/views.py
@@ -25,7 +25,7 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -51,6 +51,18 @@ def logoutUser(request):
 
 def registerPage(request):
     form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+
     return render(request, 'base/login_register.html', {'form': form})
 
 
